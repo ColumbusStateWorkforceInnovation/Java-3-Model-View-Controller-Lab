2beebf147ea6be3d498572218505253532dcf98b
ghazen-cscc <70494811+ghazen-cscc@users.noreply.github.com>
2025-04-25 12:47:13 -0500

    Update ApplicationController comment with correct package

--- src/edu/cscc/mvc/framework/ApplicationController.java
+++ src/edu/cscc/mvc/framework/ApplicationController.java
@@ -2,7 +2,7 @@ package edu.cscc.mvc.framework;
 
 /**
  * This is the base class that all Controllers should inherit from.
- * Make sure that all controllers are in the edu.cscc.mvc.withRouting package
+ * Make sure that all controllers are in the edu.cscc.mvc package
  * and end with the word "Controller".
  */
 public abstract class ApplicationController {
